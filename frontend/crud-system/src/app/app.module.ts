4f5605abd69fe20d3143af1bb2cdc5ba58e9bb3f
Guilherme Diniz <guilhermeplays03@gmail.com>
2023-11-18 14:30:21 -0300

    Implementing forms

--- frontend/crud-system/src/app/app.module.ts
+++ frontend/crud-system/src/app/app.module.ts
@@ -11,6 +11,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatListModule} from '@angular/material/list';
 import {MatIconModule} from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
   declarations: [
@@ -21,6 +25,10 @@ import {MatIconModule} from '@angular/material/icon';
     FooterComponent
   ],
   imports: [
+    MatInputModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatCardModule,
     MatIconModule,
     MatListModule,
     MatSidenavModule,
