ee09b4f8de4c353817a30260846943b2c6893347
Guilherme Diniz <guilhermeplays03@gmail.com>
2023-11-21 16:15:10 -0300

    knowing lifecycle hook angular

--- frontend/crud-system/src/app/pages/home/home.component.ts
+++ frontend/crud-system/src/app/pages/home/home.component.ts
@@ -1,10 +1,63 @@
-import { Component } from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  DoCheck,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {
+export class HomeComponent
+  implements
+    OnInit,
+    OnChanges,
+    DoCheck,
+    AfterViewInit,
+    AfterViewChecked,
+    AfterContentInit,
+    AfterViewChecked,
+    AfterContentChecked,
+    OnDestroy
+{
+  ngAfterContentChecked(): void {
+    this.text = "Conteudo alterado apos [ngAfterContentInit]";
 
+    console.log(this.text);
+  }
+
+  text: string = "";
+
+  ngOnDestroy(): void {
+    //console.log('ngOnDestroy');
+  }
+  ngAfterViewChecked(): void {
+    //console.log('ngAfterViewChecked');
+  }
+  ngAfterContentInit(): void {
+    this.text = "Conteudo alterado apos [ngAfterContentInit]";
+    //console.log('ngAfterContentInit');
+  }
+  ngAfterViewInit(): void {
+    this.text = "Conteudo alterado apos [ngAfterViewInit]"
+    console.log('ngAfterViewInit');
+  }
+  ngDoCheck(): void {
+    console.log('ngDoCheck =', this.text);
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    //console.log('ngOnChanges');
+  }
+  ngOnInit(): void {
+    console.log('ngOnInit');
+    this.text = "Conteudo inicial"
+  }
 }
