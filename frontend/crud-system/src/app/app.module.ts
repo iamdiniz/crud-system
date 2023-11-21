e312fe1b3af35c720259ea34f174d73e6913b93a
Guilherme Diniz <guilhermeplays03@gmail.com>
2023-11-21 15:20:32 -0300

    Sharing components with @Input

--- frontend/crud-system/src/app/app.module.ts
+++ frontend/crud-system/src/app/app.module.ts
@@ -17,6 +17,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
 import { LoginComponent } from './pages/login/login.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
@@ -28,6 +29,7 @@ import { LoginComponent } from './pages/login/login.component';
     LoginComponent
   ],
   imports: [
+    MatToolbarModule,
     FormsModule,
     MatInputModule,
     MatButtonModule,
